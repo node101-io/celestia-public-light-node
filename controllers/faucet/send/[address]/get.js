b9576ee2b6be0d6d9c50b72703a799ccd7a602f5
tunntun <mervehtnozcan@gmail.com>
2025-02-06 16:51:34 +0300

    fix faucet

--- controllers/faucet/send/[address]/get.js
+++ controllers/faucet/send/[address]/get.js
@@ -1,3 +1,4 @@
+import { transaction } from 'starknet';
 import config from '../../../../config.js';
 import {
   checkIPAndWalletAddressLimit,
@@ -11,26 +12,47 @@ export default (req, res) => {
   const address = req.params.address;
 
   if (!address || typeof address !== 'string' || !address.trim().length)
-    return res.send({ result: 'address is required' });
+    return res.send({
+      code: 0,
+      result: 'address is required'
+    });
 
   if (!address.startsWith(config.sender.option.prefix))
-    return res.send({ result: `Address [${address}] is not supported.` });
+    return res.send({
+      code: 0,
+      result: `Address [${address}] is not supported.`
+    });
 
   checkIPAndWalletAddressLimit(req.ip, address, (err) => {
-    if (err) return res.send({ result: 'You requested too often' });
+    if (err) return res.send({
+      code: 0,
+      result: 'You requested too often'
+    });
 
     updateRateLimitHistory(req.ip, (err, result) => {
-      if (err) return res.send({ result: 'Failed, Please contact to admin.' });
+      if (err) return res.send({
+        code: 0,
+        result: 'Failed, Please contact to admin.'
+      });
 
       sendToken(address, (err, result) => {
-        console.log(err, result);
-        if (err) return res.send({ result: 'Failed, Please contact to admin.' });
+        if (err) return res.send({
+          code: 0,
+          result: 'Failed, Please contact to admin.'
+        });
 
         updateRateLimitHistory(address, (err, ret) => {
           if (err)
-            return res.send({ result: 'Failed, Please contact to admin.' });
+            return res.send({
+              code: 0,
+              result: 'Failed, Please contact to admin.'
+            });
 
-          return res.send({ result: ret });
+          return res.send({
+            code: 1,
+            result: 'Success',
+            transactionHash: result.transactionHash
+          });
         });
       });
     });
